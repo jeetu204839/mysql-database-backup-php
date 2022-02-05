d6565323b6fea574ef0c0f31ea3d3d6275ef6345
jeetu204839 <jeeturay2@gmail.com>
2022-02-06 03:29:29 +0530

    initial commit

---
+++ export.php
@@ -0,0 +1,54 @@
+<?php
+
+$con = mysqli_connect('localhost', 'root', '', 'laravel-crm');
+
+
+$tables = array();
+
+$result = mysqli_query($con,"SHOW TABLES");
+while ($row = mysqli_fetch_row($result)) {
+	$tables[] = $row[0];
+}
+
+$return = '';
+
+foreach ($tables as $table) {
+	$result = mysqli_query($con, "SELECT * FROM ".$table);
+	$num_fields = mysqli_num_fields($result);
+
+	$return .= 'DROP TABLE '.$table.';';
+	$row2 = mysqli_fetch_row(mysqli_query($con, 'SHOW CREATE TABLE '.$table));
+	$return .= "\n\n".$row2[1].";\n\n";
+
+	for ($i=0; $i < $num_fields; $i++) { 
+		while ($row = mysqli_fetch_row($result)) {
+			$return .= 'INSERT INTO '.$table.' VALUES(';
+			for ($j=0; $j < $num_fields; $j++) { 
+				$row[$j] = addslashes($row[$j]);
+				if (isset($row[$j])) {
+					$return .= '"'.$row[$j].'"';} else { $return .= '""';}
+					if($j<$num_fields-1){ $return .= ','; }
+				}
+				$return .= ");\n";
+			}
+		}
+		$return .= "\n\n\n";
+	
+}
+
+$backup_name = date('d-m-y').'-backup.sql';
+
+$handle = fopen($backup_name, 'w+');
+fwrite($handle, $return);
+fclose($handle);
+echo "success";
+
+/*download file*/
+$file_url = $backup_name;  
+header('Content-Type: application/octet-stream');  
+header("Content-Transfer-Encoding: utf-8");   
+header("Content-disposition: attachment; filename=\"" . basename($file_url) . "\"");   
+readfile($file_url);  
+
+
+?>
